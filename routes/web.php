d0b601730c7f47b58a3a7f469cd7ca420edb6d0d
Rahageer Saadman Islam <rahageer.saadman.islam@g.bracu.ac.bd>
2025-04-19 15:50:32 +0600

    Bug fixes

--- routes/web.php
+++ routes/web.php
@@ -2,6 +2,8 @@
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\ThesisPostController;
 use App\Http\Controllers\ThesisRequestController;
+use App\Http\Controllers\Auth\LoginController;
+use App\Http\Controllers\Auth\RegisterController;
 
 Route::get('/thesis', [ThesisPostController::class, 'index']);
 Route::get('/thesis/create', [ThesisPostController::class, 'create']);
@@ -13,3 +15,14 @@ Route::post('/thesis/delete/{id}', [ThesisPostController::class, 'destroy']);
 
 Route::post('/thesis/request/{id}', [ThesisRequestController::class, 'sendRequest']);
 Route::get('/thesis/requests', [ThesisRequestController::class, 'myReceivedRequests']);
+
+// Login
+Route::get('/login', [LoginController::class, 'showLoginForm'])->name('login');
+Route::post('/login', [LoginController::class, 'login']);
+
+// Logout
+Route::post('/logout', [LoginController::class, 'logout'])->name('logout');
+
+// Register
+Route::get('/register', [RegisterController::class, 'showRegistrationForm'])->name('register');
+Route::post('/register', [RegisterController::class, 'register']);
